07f88ec172de44bfb5dde2870e7f163c9bf23c72
Sean Martin <seankieran.m@hotmail.com>
2022-09-20 18:06:25 +0100

    Final part: logging

--- napari-simple-reload/napari_simple_reload/_widget.py
+++ napari-simple-reload/napari_simple_reload/_widget.py
@@ -1,9 +1,36 @@
 # napari_simple_reload/_widget.py
 from magicgui import magic_factory
 from qtpy.QtWidgets import QVBoxLayout, QPushButton, QWidget, QLineEdit
+import logging
+import sys
+from napari.utils.notifications import notification_manager, Notification, NotificationSeverity, show_console_notification
+
+my_plugin_logger = logging.getLogger("napari_simple_reload")
+my_plugin_logger.setLevel(logging.WARNING)
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(
+    logging.Formatter(
+        fmt="%(levelname)s: %(asctime)s %(message)s",
+        datefmt="%d/%m/%Y %I:%M:%S %p"
+    )
+)
+my_plugin_logger.addHandler(stdout_handler)
+
+def show_debug(message: str):
+    """
+    Show a debug message in the notification manager.
+    """
+    notification_ = Notification(message, severity=NotificationSeverity.DEBUG)
+    # This goes to the console only ->
+    show_console_notification(notification_)
+    # This goes to the console and the napari GUI ->
+    notification_manager.dispatch(notification_)
+    # You can control what level of these messages is shown via napari preferences
 
 def example(input_string: str) -> str:
     output_string = f"You entered {input_string}!" if input_string else "Please enter something in the text box."
+    show_debug(f"The input string was (napari): {input_string}")
+    my_plugin_logger.debug(f"The input string was (logging): {input_string}")
     print(output_string)
     return output_string
 
