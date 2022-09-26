ce5db77ac2eb1e6b13f33a37c5a9e1b55c676cb9
seankmartin <martins7@tcd.ie>
2022-09-26 11:38:43 +0100

    formatting

--- napari-simple-reload/napari_simple_reload/_widget.py
+++ napari-simple-reload/napari_simple_reload/_widget.py
@@ -3,18 +3,23 @@ from magicgui import magic_factory
 from qtpy.QtWidgets import QVBoxLayout, QPushButton, QWidget, QLineEdit
 import logging
 import sys
-from napari.utils.notifications import notification_manager, Notification, NotificationSeverity, show_console_notification
+from napari.utils.notifications import (
+    notification_manager,
+    Notification,
+    NotificationSeverity,
+    show_console_notification,
+)
 
 my_plugin_logger = logging.getLogger("napari_simple_reload")
 stdout_handler = logging.StreamHandler(sys.stderr)
 stdout_handler.setFormatter(
     logging.Formatter(
-        fmt="%(levelname)s: %(asctime)s %(message)s",
-        datefmt="%d/%m/%Y %I:%M:%S %p"
+        fmt="%(levelname)s: %(asctime)s %(message)s", datefmt="%d/%m/%Y %I:%M:%S %p"
     )
 )
 my_plugin_logger.addHandler(stdout_handler)
 my_plugin_logger.setLevel(logging.WARNING)
+
 
 def show_debug(message: str):
     """
@@ -27,16 +32,23 @@ def show_debug(message: str):
     notification_manager.dispatch(notification_)
     # You can control what level of these messages is shown via napari preferences
 
+
 def example(input_string: str) -> str:
-    output_string = f"You entered {input_string}!" if input_string else "Please enter something in the text box."
+    output_string = (
+        f"You entered {input_string}!"
+        if input_string
+        else "Please enter something in the text box."
+    )
     show_debug(f"The input string was (napari): {input_string}")
     my_plugin_logger.debug(f"The input string was (logging): {input_string}")
     print(output_string)
     return output_string
 
+
 @magic_factory
 def example_factory(input_string: str) -> str:
     return example(input_string)
+
 
 class ExampleQWidget(QWidget):
     def __init__(self):
--- napari-simple-reload/test_print.py
+++ napari-simple-reload/test_print.py
@@ -1,11 +1,13 @@
 from napari_simple_reload._widget import example_factory
 
+
 def test_false_inputs():
     values_to_test = [False, 0, None]
     for value in values_to_test:
         wdg = example_factory(input_string={"value": str(value)})
         result = wdg()
         assert result == f"You entered {value}!"
-    
+
+
 if __name__ == "__main__":
     test_false_inputs()
